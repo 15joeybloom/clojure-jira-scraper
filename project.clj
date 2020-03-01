685d88800acf3c9aa4e9e2db8c565d7510c0e214
John Jacobsen <eigenhombre@gmail.com>
2020-03-01 08:49:23 -0600

    Ancient.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :main scraper.core
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cheshire "5.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cheshire "5.10.0"]
                  [clj-http "3.10.0"]
                  [environ "1.1.0"]
                  [hickory "0.7.1"]]
